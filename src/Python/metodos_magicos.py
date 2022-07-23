950646a2c9ea64352a8d309c2e178f89083873a7
Thiago Siqueira <thiagossiqueira@gmail.com>
2022-07-23 17:19:43 -0300

    metodos magicos

--- src/Python/metodos_magicos.py
+++ src/Python/metodos_magicos.py
@@ -1,3 +1,35 @@
+#É intuitivo deduzir o que acontece quando fazemos int + int, ou quando mandamos imprimir um float. Mas observe que certos dados em Python possuem comportamentos "surpreendentes" e úteis. Por exemplo, ao darmos print em uma lista, ela aparece representada com colchetes e vírgulas separando os dados. Quando somamos 2 strings, elas são concatenadas.
+
+#Chamamos de SOBRECARGA DE OPERADORES o ato de ensinarmos ao nosso programa como ele deve aplicar um certo operador sobre os objetos de uma certa classe. Para sobrecarregarmos operadores em Python, utilizamos os "métodos mágicos": funções padrão que o Python irá ligar a certos operadores. Vejamos alguns dos mais comuns:
+
+#_add_: soma
+
+#_sub_: subtração
+
+#_mul_: multiplicação
+
+#_truediv_: divisão
+
+#_mod_: resto da divisão
+
+#_pow_: expoente
+
+#_lt_: "less than" <
+
+#_le_: "less or equal" <=
+
+#_gt_: "greater than" >
+
+#_ge_: "greater or equal" >=
+
+#_eq_: "equal" ==
+
+#_ne_: "not equal" !=
+
+#_repr_: a string retornada por esse método é utilizada pelo print
+
+#Vejamos um exemplo com uma grandeza vetorial de 2 dimensões, representada por 2 coordenadas i e j ex: 2i + 4j Vamos criar uma classe e usar alguns métodos mágicos pra decidir como somar e subtrair 2 vetores, comparar se são iguais ou diferentes e imprimir na forma bonitinha acima.
+
 class Vetor2Dimensoes:
    def __init__(self, i, j):
        self.i = i
